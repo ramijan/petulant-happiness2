7454bf6cf18be5c342295ecf6a70042c736310c4
AlexMasonAM <alexmmason@gmail.com>
2015-01-26 12:36:44 -0800

    added Artist model

---
+++ app/models/artist.rb
@@ -0,0 +1,14 @@
+class Artist
+  include Mongoid::Document
+  
+  
+
+  field :first_name, type: String
+  field :last_name, type: String
+  field :bandname, type: String
+  field :genre, type: String
+
+  #relational
+  has_many :guitars
+
+end
